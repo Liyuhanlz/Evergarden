a3524f22863e39261f517efb70d4f6fec8f633d8
Liyuhanlz <liyuhanlz09@gmail.com>
2026-03-25 07:21:50 -0700

    working on scriptable objects

--- Assets/Scripts/PlantGrow.cs
+++ Assets/Scripts/PlantGrow.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "PlantGrow", menuName = "Scriptable Objects/PlantGrow")]
 public class PlantGrow : ScriptableObject
 {
